ebc8d84e6886e91df89bcac7c3ce3470930f1a5e
Jadit19 <jadit19@gmail.com>
2022-05-22 11:34:32 +0530

    update flask server

--- server_flask/image_comparer.py
+++ server_flask/image_comparer.py
@@ -13,14 +13,14 @@ class ImageComparer():
         self.load_data()
 
     def load_data(self):
-        print("Loading data..")
+        print("\n[flask] Loading data..")
         for (root, dirs, files) in os.walk('./images'):
             self.files.extend(files)
         for file in self.files:
             face = fr.load_image_file("./images/" + file)
             self.known_names.append(re.sub("[0-9]",'', file[:-4]))
             self.face_encodings.append(fr.face_encodings(face)[0])
-        print("Data Loaded successfully!")
+        print("[flask] Data Loaded successfully!\n")
 
     def get_name(self, base_64_img):
         decoded_img = ur.urlopen(base_64_img)
@@ -43,5 +43,5 @@ class ImageComparer():
         json_to_export = {}
         json_to_export['name'] = self.get_name(base_64_img)
         json_to_export['hour'] = f'{time.localtime().tm_hour}:{time.localtime().tm_min}'
-        json_to_export['date'] = f'{time.localtime().tm_year}-{time.localtime().tm_mon}-{time.localtime().tm_mday}'
+        json_to_export['date'] = f'{time.localtime().tm_mday}/{time.localtime().tm_mon}/{time.localtime().tm_year}'
         return json_to_export
