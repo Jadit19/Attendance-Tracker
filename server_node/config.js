77cedc4fb2ac6e099da05a0726f779dbf26beba3
harshulmidha <harshulmidha@gmail.com>
2022-05-27 14:52:23 +0530

    minor updates

--- server_node/config.js
+++ server_node/config.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const PORT = process.env.NODE_SERVER_PORT
-const MONGO_URL = process.env.MONGO_URL
 const PASSWORD = String(process.env.PASSWORD)
 
-module.exports = { PORT, MONGO_URL, PASSWORD }
+module.exports = { PORT, PASSWORD }
